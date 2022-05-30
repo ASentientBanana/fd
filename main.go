1da6cdb12f07aebd0cee807c2a4c88f42a0c402a
ASentientBanana <asentientbanana@gmail.com>
2022-05-30 16:13:22 +0200

    new functionality

--- main.go
+++ main.go
@@ -1,120 +1,47 @@
 package main
 
 import (
-	"bufio"
 	"errors"
 	"flag"
-	"fmt"
-	"io"
-	"os"
-	"strings"
+
+	"github.com/ASentientBanana/fd/util"
 )
 
-func Copy(src, dest string) (int64, error) {
-	origin, err := os.Open(src)
-	if err != nil {
-		return 0, err
-	}
-	defer origin.Close()
-
-	destinationFile, destErr := os.Create(dest)
-	if destErr != nil {
-		return 0, err
-	}
-	defer destinationFile.Close()
-
-	nBytes, err := io.Copy(destinationFile, origin)
-	return nBytes, err
-}
-
-func Move(src, dest string) (int64, error) {
-	origin, err := os.Open(src)
-	if err != nil {
-		return 0, err
-	}
-	defer origin.Close()
-
-	destinationFile, destErr := os.Create(dest)
-	if destErr != nil {
-		return 0, err
-	}
-	defer destinationFile.Close()
-
-	nBytes, err := io.Copy(destinationFile, origin)
-	os.Remove(src)
-	return nBytes, err
-}
-
-func truncate_file(filePath string) error {
-	if err := os.Truncate(filePath, 0); err != nil {
-		return err
-	}
-	return nil
-}
-
-func ErrorCleanup() {
-	fmt.Println("Something went wrong")
-}
-
-func Replace(path, target, new_word string) {
-	split_string := strings.Split(path, "/")
-	tmp_file_path := strings.Join(split_string[:len(split_string)-1], "/") + ".tmp_file"
-	_, cErr := Copy(path, tmp_file_path)
-	if cErr != nil {
-		panic(cErr)
-	}
-	tmp_file, tErr := os.OpenFile(tmp_file_path, os.O_RDWR, 0644)
-	file, err := os.OpenFile(path, os.O_RDWR, 0644)
-	if err != nil || tErr != nil {
-		panic(err)
-	}
-
-	defer file.Close()
-	defer tmp_file.Close()
-
-	scan := bufio.NewScanner(tmp_file)
-	new_file_data := []string{}
-	for scan.Scan() {
-		line := strings.ReplaceAll(scan.Text(), target, new_word)
-		new_file_data = append(new_file_data, line+"\n")
-	}
-	truncate_file(path)
-
-	for i := 0; i < len(new_file_data); i++ {
-		_, wErr := file.WriteString(new_file_data[i])
-		if wErr != nil {
-			panic(wErr)
-		}
-	}
-	defer os.Remove(tmp_file_path)
-}
-
-func validate(replace, copy, move *bool) {
+func validate(replace, copy, move, truncate, clone_into *bool) {
 	if flag.NArg() == 0 {
 		panic(errors.New("Error no arguments or flags provided"))
 	}
 	if *replace && flag.NArg() == 3 {
-		Replace(flag.Arg(0), flag.Arg(1), flag.Arg(2))
+		util.Replace(flag.Arg(0), flag.Arg(1), flag.Arg(2))
 	}
 	if *copy && flag.NArg() == 2 {
-		Copy(flag.Arg(0), flag.Arg(1))
+		util.Copy(flag.Arg(0), flag.Arg(1))
 	}
 	if *move && flag.NArg() == 2 {
-		Move(flag.Arg(0), flag.Arg(1))
+		util.Move(flag.Arg(0), flag.Arg(1))
 	}
+	if *truncate && flag.NArg() == 1 {
+		util.Truncate_file(flag.Arg(0))
+	}
+	if *clone_into && flag.NArg() == 2 {
+		util.Clone_into(flag.Arg(0), flag.Arg(1))
+	}
+
 }
 
 func main() {
 
 	replace := flag.Bool("r", false, "Replace all string occurrences in a given file: path_to_string;new_string;target_string")
 	copy := flag.Bool("c", false, "Copy file from one path 1 to path2")
+	clone_into := flag.Bool("ci", false, "Copy file contents into a different file")
 	move := flag.Bool("m", false, "Move file from one path 1 to path2")
+	truncate := flag.Bool("t", false, "Empty out file")
 	flag.Parse()
 
 	if flag.NFlag() > 1 {
 		panic(errors.New("Only one flag supported currently."))
 	}
 
-	validate(replace, copy, move)
+	validate(replace, copy, move, truncate, clone_into)
 
 }
---
+++ util/file.go
@@ -0,0 +1,117 @@
+package util
+
+import (
+	"bufio"
+	"io"
+	"os"
+	"strings"
+)
+
+func Copy(src, dest string) (int64, error) {
+	origin, err := os.Open(src)
+	if err != nil {
+		return 0, err
+	}
+	defer origin.Close()
+
+	destinationFile, destErr := os.Create(dest)
+	if destErr != nil {
+		return 0, err
+	}
+	defer destinationFile.Close()
+
+	nBytes, err := io.Copy(destinationFile, origin)
+	return nBytes, err
+}
+
+func Move(src, dest string) (int64, error) {
+	origin, err := os.Open(src)
+	if err != nil {
+		return 0, err
+	}
+	defer origin.Close()
+
+	destinationFile, destErr := os.Create(dest)
+	if destErr != nil {
+		return 0, err
+	}
+	defer destinationFile.Close()
+
+	nBytes, err := io.Copy(destinationFile, origin)
+	os.Remove(src)
+	return nBytes, err
+}
+
+func Truncate_file(filePath string) error {
+	if err := os.Truncate(filePath, 0); err != nil {
+		return err
+	}
+	return nil
+}
+
+func ErrorCleanup(err error) {
+	panic(err)
+}
+
+func file_text_to_slice(file io.Reader) []string {
+	scan := bufio.NewScanner(file)
+	new_file_data := []string{}
+	for scan.Scan() {
+		new_file_data = append(new_file_data, scan.Text()+"\n")
+	}
+	return new_file_data
+}
+func write_file_from_array(file_contents_array []string, file *os.File) error {
+	for i := 0; i < len(file_contents_array); i++ {
+		_, wErr := file.WriteString(file_contents_array[i])
+		if wErr != nil {
+			return wErr
+		}
+	}
+	return nil
+}
+
+func Replace(path, target, new_word string) {
+	split_string := strings.Split(path, "/")
+	tmp_file_path := strings.Join(split_string[:len(split_string)-1], "/") + ".tmp_file"
+	_, cErr := Copy(path, tmp_file_path)
+	if cErr != nil {
+		panic(cErr)
+	}
+	tmp_file, tErr := os.OpenFile(tmp_file_path, os.O_RDWR, 0644)
+	file, err := os.OpenFile(path, os.O_RDWR, 0644)
+	if err != nil || tErr != nil {
+		panic(err)
+	}
+
+	defer file.Close()
+	defer tmp_file.Close()
+
+	scan := bufio.NewScanner(tmp_file)
+	new_file_data := []string{}
+	for scan.Scan() {
+		line := strings.ReplaceAll(scan.Text(), target, new_word)
+		new_file_data = append(new_file_data, line+"\n")
+	}
+	Truncate_file(path)
+
+	write_err := write_file_from_array(new_file_data, file)
+	if write_err != nil {
+		ErrorCleanup(write_err)
+	}
+	defer os.Remove(tmp_file_path)
+}
+
+func Clone_into(source_path, target_path string) {
+	Truncate_file(target_path)
+	source_file, source_err := os.OpenFile(source_path, os.O_RDWR, 0644)
+	target_file, target_err := os.OpenFile(target_path, os.O_RDWR, 0644)
+	if source_err != nil || target_err != nil {
+		ErrorCleanup(source_err)
+	}
+	file_contents := file_text_to_slice(source_file)
+	write_err := write_file_from_array(file_contents, target_file)
+	if write_err != nil {
+		ErrorCleanup(write_err)
+	}
+}
